5ea3d3c7169e236e135ed2125611234fb9456ba1
hemanthkone <151901445+hemanthkone@users.noreply.github.com>
2024-10-15 12:41:41 +0530

    Create Arithmetic.java

---
+++ Arithmetic.java
@@ -0,0 +1,22 @@
+//java programe to show the fibinocci series
+/*
+file name: Arithmetic.java
+date;09/10/2024
+author:hemaanth
+*/
+import java.io.*;
+import java.util.*;
+public class Arithmetic
+{
+public static void main(String[] args)
+{
+   int x=6;
+   int y=3;
+   int A=x+y,S=x-y,M=x*y,D=x/y;
+System.out.println("Result of Addition       "+x+" and "+y+" is "+A);
+System.out.println("Result of Subtraction    "+x+" and "+y+" is "+S);
+System.out.println("Result of Multiplication "+x+" and "+y+" is "+M);
+System.out.println("Result of Division       "+x+" and "+y+" is "+D);
+}
+
+}
