164d94a773da963c8d6fe5e64bb7249666bc1d99
hemanthkone <151901445+hemanthkone@users.noreply.github.com>
2024-10-15 11:31:42 +0530

    Create DoWhileDemo.java

---
+++ DoWhileDemo.java
@@ -0,0 +1,14 @@
+//java program to  demonstrate do while loop 
+/*file name:DoWhileDemo.java
+date:23/08/2024
+author:hemanth*/
+import java.io.*;
+public class DoWhileDemo{
+    public static void main(String args[]){
+        int i=0;
+        do{
+            System.out.println("Hello world");
+            i++;
+        }while(i<3);
+    }
+} 
